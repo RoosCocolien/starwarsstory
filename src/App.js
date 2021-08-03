d06d8a12ebbf9f02b344bb672e5db9eee2b294f9
Roos Cocolien <r.steigenga@gmail.com>
2021-08-03 11:56:47 +0200

    added react-router-dom to App.js for navigation and components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+//routing
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from 'react-router-dom';
+//styles (hier komt een GlobalStyle Component)
 import './App.css';
+//components
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Story from './components/Story';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-		<header className="App-header">
-			<h1>
-				STAR WARS
-			</h1>
-			<p>
-				An interactive Star Wars Story
-			</p>
-		</header>
-    </div>
+    <Router>
+		<Navigation />
+		<Switch>
+			<Route exact path='/' component={Home}></Route>
+			<Route exact path='/story' component={Story}></Route>
+			<Route exact path='/about' component={About}></Route>
+			<Route exact path='/contact' component={Contact}></Route>
+		</Switch>
+		<Footer />
+	</Router>
   );
 }
 
